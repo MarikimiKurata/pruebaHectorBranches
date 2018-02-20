d525615d2012ad400e847a3bef89321df847bace
Mari Kimi <0148421@up.edu.mx>
2018-02-19 23:54:07 -0600

    subir variables

---
+++ variables.playground/Contents.swift
@@ -0,0 +1,83 @@
+// -------------------------------------------------------------------------------Sección 2: Variables
+// variable string
+var str: String = "hola" //: String
+
+// variable number (Int)
+var age: Int = 26        // :  Int
+var pi: Float = 3.1416     //: Float
+
+// variables constantes, no pueden cambiar su valor
+
+let añoDeNacimiento = 1991
+let PI = 3.1416
+
+// llamar variables
+str
+age
+pi
+
+// modificar variables (las contasntes no puden ser modificadas)
+var num = 4
+num = 5
+num
+num = 80
+num
+
+//agregar variable dentro de un string con \(variable)
+str = "hola tengo \(num) de años"
+
+// -------------------------------------------------------------------------------Sección 4: String
+//inicializar variable vacio (asignar un valor de una variable despues de declararla)
+var idUser: String
+idUser = "09876526292"
+
+//Myusculas
+var mayusculas: String = "marikimi"
+mayusculas.uppercased()
+
+//Minusculas
+var minusculas: String = "MARIKIMI"
+minusculas.lowercased()
+
+//Emojis
+var emoji = "💍" // emogi= comand + control + space
+
+// -------------------------------------------------------------------------------Sección 4: numeros enteros
+var edad: Int
+edad = 26
+
+//Operaciones
+edad + 5
+edad - 5
+edad * 2
+edad / 2
+
+//sumar o restar por unidad (i++)
+edad += 1
+edad += 1
+edad += 1
+edad += 1
+
+edad -= 1
+edad -= 1
+edad -= 1
+edad -= 1
+
+// -------------------------------------------------------------------------------Sección 4: string de interpolacion \(valor)
+let name = "Marikimi"
+let años = 15
+let icon = "🐰"
+
+let presentación = "Hola, me llamo \(name) y tengo \(años) \(icon)"
+
+
+
+
+
+
+
+
+
+
+
+
